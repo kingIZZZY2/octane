a5242c594b1fc581d4abe2999d508cac4cba7803
kingIZZZY2 <logic.cpp+github2@gmail.com>
2022-05-18 05:05:28 -0400

    trying loop/wait/check octane table value approach; remove server/waitgroup approach

--- src/Worker.php
+++ src/Worker.php
@@ -88,8 +88,8 @@ class Worker implements WorkerContract
 
         $gateway = new ApplicationGateway($this->app, $sandbox);
 
-        $waitgroup = &$context->waitgroup; // necessary?
-        $request->attributes->set('waitgroup', $waitgroup);
+        // $waitgroup = &$context->waitgroup; // necessary?
+        // $request->attributes->set('waitgroup', $waitgroup);
 
         try {
             $responded = false;
